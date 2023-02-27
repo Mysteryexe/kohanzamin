4165765e58435f93d66bad7524d80e17362bcef8
Mysteryexe <amiralipourmehdighaemmaghami@gmail.com>
2023-02-27 11:38:00 +0330

    update

--- content/js/intro.js
+++ content/js/intro.js
@@ -1,4 +1,17 @@
 const timeline = document.getElementsByTagName("timeline")[0];
+const ptrs = document.getElementsByTagName("ptr")
+
+function animateBars(x){
+  x =( x / window.innerHeight)* window.innerHeight/10
+  setTimeout(() => {
+    ptrs[0].style.backgroundPosition= `0% ${x}px`
+    ptrs[1].style.backgroundPosition= `0% ${x}px`
+    ptrs[2].style.backgroundPosition= `${x}px 0%`
+    ptrs[3].style.backgroundPosition= `${x}px 0%`
+  }, 20);
+
+}
+
 function intro() {
   let distance = window.scrollY;
   let passed = (distance + window.innerHeight) / timeline.offsetHeight;
@@ -6,6 +19,7 @@ function intro() {
     timeline.classList.add("done");
   } else {
     timeline.classList.remove("done");
+    animateBars(distance);
   }
   // personMove(distance);
 }
@@ -14,6 +28,7 @@ function intro() {
 //   distance = parseInt(distance / 100);
 //   personImg.src = `/new/frame${distance % 7}.png`;
 // }
+
 window.addEventListener("scroll", function () {
   intro();
 });
