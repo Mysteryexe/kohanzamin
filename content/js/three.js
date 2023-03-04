fd2aa3c1c6a42b3a96ef2570c2d1b64204bd91ce
Mysteryexe <amiralipourmehdighaemmaghami@gmail.com>
2023-03-04 20:10:13 +0330

    3D shape

--- content/js/three.js
+++ content/js/three.js
@@ -1,7 +1,11 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+//wd
+import * as THREE from './three.module.js';
+import { GLTFLoader } from './GLTFLoader.js';
 
 const intro = document.getElementsByTagName("intro")[0];
 const timeline = document.getElementsByTagName("timeline")[0];
+console.log("in")
+const loader = new GLTFLoader();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, intro.offsetWidth / intro.offsetHeight, 0.1, 1000 );
@@ -10,30 +14,44 @@ const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize( intro.offsetWidth, intro.offsetHeight );
 const element = intro.appendChild( renderer.domElement );
 element.classList.add("window")
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0xDC8032 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-const edges = new THREE.EdgesGeometry( geometry );
-const cube = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xFFFFFF } ) );
-scene.add( cube );
-
-
 camera.position.z = 5;
 
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// // const material = new THREE.MeshBasicMaterial( { color: 0xDC8032 } );
+// // const cube = new THREE.Mesh( geometry, material );
+// // scene.add( cube );
+// const edges = new THREE.EdgesGeometry( geometry );
+// const cube = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xFFFFFF } ) );
+// scene.add( cube );
+loader.load( '/content/glb/tagh.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+    const mesh = scene.children[ 0 ]
+    scene.add(mesh)
+    console.log(mesh)
+    mesh.scale.x = 0.05
+    mesh.scale.y = 0.05
+    mesh.scale.z = 0.05
+    mesh.position.set(0,-1.5,0)
+    animate()
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
 function animate() {
-    // console.log(passed)
+    const mesh = scene.children[ 0 ]
     let passed = (window.scrollY + window.innerHeight) / timeline.offsetHeight;
+    if (passed > 1){
+        passed =1
+    }
     renderer.setSize( intro.offsetWidth, intro.offsetHeight );
     requestAnimationFrame( animate );
 
-    cube.rotation.x = 100*passed;
-    cube.rotation.y = 100*passed;
-    cube.scale.x = -5*passed;
-    cube.scale.y = -5*passed;
-    cube.scale.z = -5*passed;
+    mesh.rotation.y = 50*passed;
+    mesh.rotation.x = 1*passed;
+    // mesh.scale.x = 0.5/passed;
+    // mesh.scale.y = 0.5/passed;
+    // mesh.scale.z = 0.5/passed;
     renderer.render( scene, camera );
 }
-
-animate();
